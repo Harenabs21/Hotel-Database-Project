70e0b92e0c34f44302f19718fda91b852bc9d16c
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-07-15 13:20:52 +0300

    feat: add Request folder and easy-line12.sql

---
+++ Requests/easy/easy-line12.sql
@@ -0,0 +1,25 @@
+-- DESPLAY ALL ROOM ORDER BY PRICE DESC:
+-->
+    SELECT r.number, r.room_type, r.capacity_room, h.name AS hotel, p.cost_per_night,
+        CONCAT_WS(', ',
+            CASE WHEN rf.sea_view = true THEN 'sea_view' ELSE NULL END,
+            CASE WHEN rf.VIP_category = true THEN 'VIP_category' ELSE NULL END,
+            CASE WHEN rf.hot_water = true THEN 'hot_water' ELSE NULL END,
+            CASE WHEN rf.wifi_available = true THEN 'wifi_available' ELSE NULL END,
+            CASE WHEN rf.room_service = true THEN 'room_service' ELSE NULL END,
+            CASE WHEN rf.mini_bar = true THEN 'mini_bar' ELSE NULL END,
+            CASE WHEN rf.flat_screen = true THEN 'flat_screen' ELSE NULL END
+        ) AS features
+        FROM room r
+        INNER JOIN price p ON r.id_price = p.id
+        INNER JOIN hotel h ON h.id = r.id_hotel
+        INNER JOIN room_features rf ON rf.id = r.id_room_features
+        ORDER BY p.cost_per_night DESC;
+
+                               -->
+
+
+-- NOTE:
+-- Since the question didn't specify which collones should
+-- be returned, so this select returns all the information
+-- in the room table
