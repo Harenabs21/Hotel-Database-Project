7670934dc1ec979d74057cd048c14f329bda8de0
Daris02 <hei.raymond.2@gmail.com>
2023-07-15 13:14:00 +0300

    fix: alter table hotel

--- implementation/createTable.sql
+++ implementation/createTable.sql
@@ -25,7 +25,10 @@ CREATE TABLE "hotel"(
     id_province_avaible INT REFERENCES province_avaible(id)  
 );
 -- Alter table "hotel"
-    -- id INT
+ALTER TABLE "hotel"
+    MODIFY COLUMN id ;
+ALTER TABLE "hotel"
+    ADD COLUMN id_province INT REFERENCES province(id);
 
 
 ---------------------------------------------------
