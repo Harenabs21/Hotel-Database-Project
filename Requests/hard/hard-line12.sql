b0562e7aeb84ac56c4a7e6bb7169b05f6c4286aa
Daris02 <hei.raymond.2@gmail.com>
2023-07-21 15:23:23 +0300

    fix: update condition

--- Requests/hard/hard-line12.sql
+++ Requests/hard/hard-line12.sql
@@ -1,16 +1,15 @@
 -- Le nombre de reservation acceuilli par hotel 
 -- sur chaque mois d'une annee donner
 SELECT
-    avg(re.id) as reservation,
-    (
-        SELECT 
-            date_part('month', date_arrived) as month
-        FROM "reservation"
-        WHERE date_part('year', date_arrived) = '2023' -- ?
-        GROUP BY month
-    ),
+    round(avg(re.id)) as reservation,
     ho.id as hotel
 FROM "reservation" re
     INNER JOIN "room" ro ON re.id_room = ro.id
     INNER JOIN "hotel" ho ON ro.id_hotel = ho.id
-GROUP BY ho.id, month;
+WHERE date_part('month', re.date_arrived) IN (
+        SELECT 
+            date_part('month', date_arrived) as month
+        FROM "reservation"
+        WHERE date_part('year', date_arrived) = '2023' -- ?
+    )
+GROUP BY ho.id;
