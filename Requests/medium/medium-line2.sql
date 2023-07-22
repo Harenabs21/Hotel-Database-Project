1b7a0c5e25c2d6cc22abddd09ed66b4ce95ad9f4
Sheddy <hei.sheddy.2@gmail.com>
2023-07-22 15:08:10 -0700

    feat : add request medium-line2

---
+++ Requests/medium/medium-line2.sql
@@ -0,0 +1,6 @@
+-------- Afficher la liste des chambres qui seront libres demain
+SELECT r.number, r.room_type, r.capacity_room
+FROM room r
+LEFT JOIN reservation res ON res.id_room = r.id 
+AND res.leaving_date = CURRENT_DATE + INTERVAL '1 day'
+WHERE res.id_room IS NULL;
