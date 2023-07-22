322df2ec1f51e916a55a91ca785448db929f050d
Daris02 <hei.raymond.2@gmail.com>
2023-07-22 16:19:47 +0300

    docs: rewrite description

--- Requests/hard/hard-line9.sql
+++ Requests/hard/hard-line9.sql
@@ -1,11 +1,12 @@
--- Total de reservation pour chaque reservation par hotel
-SELECT
-    pr.name as promotion,
-    ho.name as hotel,
-    count(re.id) as reservation
-FROM "affiliate" aff
-    INNER JOIN "room" ro ON aff.id_room = ro.id
-    INNER JOIN "reservation" re ON re.id_room = ro.id
-    INNER JOIN "promotion" pr ON aff.id_promotion = pr.id
-    INNER JOIN "hotel" ho ON ro.id_hotel = ho.id
-GROUP BY pr.id, ho.id;
+-- Total booking for each hotel reservation
+    -->
+        SELECT
+            pr.name as promotion,
+            ho.name as hotel,
+            count(re.id) as reservation
+        FROM "affiliate" aff
+            INNER JOIN "room" ro ON aff.id_room = ro.id
+            INNER JOIN "reservation" re ON re.id_room = ro.id
+            INNER JOIN "promotion" pr ON aff.id_promotion = pr.id
+            INNER JOIN "hotel" ho ON ro.id_hotel = ho.id
+        GROUP BY pr.id, ho.id;
