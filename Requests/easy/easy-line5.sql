7e395ef167150a3ba22d41bbea5b10154113f3d0
Daris02 <hei.raymond.2@gmail.com>
2023-07-22 16:25:00 +0300

    docs: rewrite description

--- Requests/easy/easy-line5.sql
+++ Requests/easy/easy-line5.sql
@@ -1,6 +1,13 @@
---------- Afficher les promotions en fonction de la saison (periode), tout hotel confondu
-    SELECT p.name, s.items, s.start_date, s.end_date FROM promotion p 
-    INNER JOIN affiliate a ON a.id_promotion = p.id
-    INNER JOIN room ON a.id_room = room.id 
-    INNER JOIN price ON room.id_price = price.id 
-    INNER JOIN season s ON price.id_season = s.id GROUP BY s.start_date ASC;
+-- DISPLAY SPECIAL OFFERS BY SEASON (PERIOD), ALL HOTEL COMBINED
+    -->
+        SELECT
+            p.name,
+            s.items,
+            s.start_date,
+            s.end_date
+        FROM promotion p
+            INNER JOIN affiliate a ON a.id_promotion = p.id
+            INNER JOIN room ON a.id_room = room.id
+            INNER JOIN price ON room.id_price = price.id
+            INNER JOIN season s ON price.id_season = s.id
+        GROUP BY s.start_date ASC;
