b02ea94615e05ead99942b7b050aea4d36d0e380
Sheddy <hei.sheddy.2@gmail.com>
2023-07-22 15:51:23 -0700

    fix : comment the requests

--- Requests/medium/medium-line2.sql
+++ Requests/medium/medium-line2.sql
@@ -3,4 +3,7 @@ SELECT r.number, r.room_type, r.capacity_room
 FROM room r
 LEFT JOIN reservation res ON res.id_room = r.id 
 AND res.leaving_date = CURRENT_DATE + INTERVAL '1 day'
-WHERE res.id_room IS NULL;
+WHERE res.id_room IS NULL; 
+
+-- NOTE :
+--- '1 day' indicates that the next day
