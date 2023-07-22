4386b3896953cf94e1c02f7132756c39ef4aeaf0
Daris02 <hei.raymond.2@gmail.com>
2023-07-22 16:31:26 +0300

    docs: rewrite description

--- Requests/medium/medium-line1.sql
+++ Requests/medium/medium-line1.sql
@@ -1,9 +1,9 @@
--- Combien de fois un client a reserver dans nos hotel
-    SELECT
-        count(*)
-    FROM "reservation" re
-    WHERE re.id_customer = 45; -- <-- Param
-
+-- HOW MANY TIMES A CUSTOMER HAS BOOKED IN OUR HOTEL
+    -->
+        SELECT
+            count(*)
+        FROM "reservation" re
+        WHERE re.id_customer = 45; -- <-- Param
 
 -- NOTE :
--- *Param : paramatre qu'on peut changer
+-- *Param : changeable parameters
