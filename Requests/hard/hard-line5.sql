cd3e0b10941fc40656951fa835326dc764622699
Sheddy <hei.sheddy.2@gmail.com>
2023-07-22 17:53:33 -0700

    fix : check queries

--- Requests/hard/hard-line5.sql
+++ Requests/hard/hard-line5.sql
@@ -1,12 +1,14 @@
--- Total de paiement encaise au cours d'une annee pour chaque hotel
-SELECT
-    sum(pay.amount_paid) as total_in_year,
-    ho.name
-FROM "payment" pay
-    INNER JOIN receptionist re ON pay.id_receptionist = re.id
-    INNER JOIN hotel ho ON ho.id = re.id_hotel
-WHERE date_part('year', pay.payment_date) = '2023' -- <-- Param
-GROUP BY ho.id, re.id;
+------ TOTAL PAYMENT COLLECTED DURING A YEAR FOR EACH HOTEL
+--->
+    SELECT
+        sum(pay.amount_paid) as total_in_year,
+        ho.name
+    FROM "payment" pay
+        INNER JOIN receptionist re ON pay.id_receptionist = re.id
+        INNER JOIN hotel ho ON ho.id = re.id_hotel
+    WHERE date_part('year', pay.payment_date) = '2023' -- <-- Param
+    GROUP BY ho.id, re.id;
+                        --->
 
 
 -- NOTE :
