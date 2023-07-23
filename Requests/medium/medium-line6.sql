a34737430f17c5392d44a97688b10a51ca5bd80c
Sheddy <hei.sheddy.2@gmail.com>
2023-07-22 17:44:23 -0700

    fix : check queries

--- Requests/medium/medium-line6.sql
+++ Requests/medium/medium-line6.sql
@@ -1,8 +1,9 @@
---Afficher le nombre total des réservations par hotel
-
+----- SHOW THE TOTAL NUMBER OF RESERVATIONS PER HOTEL
+--->
     SELECT h.name, 
         COUNT(*) AS total_reservations
     FROM hotel h
     INNER JOIN room r ON h.id = r.id_hotel
     INNER JOIN reservation res ON r.id = res.id_room
     GROUP BY h.name;
+                    --->
