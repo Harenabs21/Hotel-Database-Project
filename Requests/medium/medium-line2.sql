b0deb957c72a879f979854215e4cb31127a78c84
Sheddy <hei.sheddy.2@gmail.com>
2023-07-22 17:28:07 -0700

    fix : check queries

--- Requests/medium/medium-line2.sql
+++ Requests/medium/medium-line2.sql
@@ -1,8 +1,10 @@
--------- Afficher la liste des chambres qui seront libres demain
+-------- SHOW THE LIST OF ROOMS THAT WILL BE FREE TOMORROW
+--->
     SELECT r.number, r.room_type, r.capacity_room FROM room r
     LEFT JOIN reservation res ON res.id_room = r.id 
     AND res.leaving_date = CURRENT_DATE + INTERVAL '1 day'
     WHERE res.id_room IS NULL; 
+                            --->
 
 -- NOTE :
 --- '1 day' indicates that the next day
